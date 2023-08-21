84e889b53fe989989915b9bc5dbb703b99856466
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-20 19:05:52 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game { "gta5" }
+
+author 'Zaps6000'
+version '1.0.0'
+
+
+client_script 'greenzone.lua'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+	'ui/*'
+}
